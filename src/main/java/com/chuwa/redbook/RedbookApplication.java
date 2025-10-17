be2560f28f0014974afa03e9c82387ea8935d16b
jialexu <jix012@ucsd.edu>
2025-10-17 15:46:17 -0700

    hw9, modified pom.xml to enable war packaging, working fine on tomcat 9.0.111

--- src/main/java/com/chuwa/redbook/RedbookApplication.java
+++ src/main/java/com/chuwa/redbook/RedbookApplication.java
@@ -2,12 +2,19 @@ package com.chuwa.redbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RedbookApplication {
+public class RedbookApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RedbookApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // tells Tomcat which class to load when deploying WAR
+        return builder.sources(RedbookApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(RedbookApplication.class, args);
+    }
 }
